960fa0a1f0eb7f25f5423108c1e63af6cbb02385
spanchwatikar <109857128+spanchwatikar@users.noreply.github.com>
2022-08-22 04:18:20 +0530

    Initial commit

---
+++ src/main/java/com/indiaugury/springbootdocker/SpringbootDockerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.indiaugury.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestC
+public class SpringbootDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
+	}
+
+}
